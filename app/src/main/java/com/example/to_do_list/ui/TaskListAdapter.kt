1f4fbaf51d04e97fe2916feeff4c7fe16ef7078c
GabrielNatan2001 <gabrielnatan.2001.pereira@gmail.com>
2022-02-05 12:08:01 -0300

    Adiciona layout do recyclerView

---
+++ app/src/main/java/com/example/to_do_list/ui/TaskListAdapter.kt
@@ -0,0 +1,7 @@
+package com.example.to_do_list.ui
+
+import android.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.to_do_list.databinding.ItemTaskBinding
+import com.example.to_do_list.model.Task
+
