6c2b38e38e82389a526fe304ef53bb3126718cf0
GabrielNatan2001 <gabrielnatan.2001.pereira@gmail.com>
2022-02-05 11:36:01 -0300

    Adiciona evento calendario e relogio em ActivityAddTask

---
+++ app/src/main/java/com/example/to_do_list/datasource/TaskDataSource.kt
@@ -0,0 +1,13 @@
+package com.example.to_do_list.datasource
+
+import com.example.to_do_list.model.Task
+
+object TaskDataSource{
+    private val list = arrayListOf<Task>()
+
+    fun getList() = list
+
+    fun insertTask(task: Task){
+        list.add(task.copy(id = list.size + 1))
+    }
+}
